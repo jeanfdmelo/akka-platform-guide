56398178894846b097953359c2648c6fe3958e31
Jean <jeanfdmelo@gmail.com>
2021-04-25 09:37:21 +0200

    Complete Event Sourced entity

--- shopping-cart-service/src/main/java/shopping/cart/ShoppingCartServiceImpl.java
+++ shopping-cart-service/src/main/java/shopping/cart/ShoppingCartServiceImpl.java
@@ -16,53 +16,107 @@ import shopping.cart.proto.*;
 
 public final class ShoppingCartServiceImpl implements ShoppingCartService {
 
-    private final Logger logger = LoggerFactory.getLogger(getClass());
+  private final Logger logger = LoggerFactory.getLogger(getClass());
 
-    private final Duration timeout;
-    private final ClusterSharding sharding;
+  private final Duration timeout;
+  private final ClusterSharding sharding;
 
-    public ShoppingCartServiceImpl(ActorSystem<?> system) {
-        timeout = system.settings().config().getDuration("shopping-cart-service.ask-timeout");
-        sharding = ClusterSharding.get(system);
+  public ShoppingCartServiceImpl(ActorSystem<?> system) {
+    timeout = system.settings().config().getDuration("shopping-cart-service.ask-timeout");
+    sharding = ClusterSharding.get(system);
+  }
+
+  @Override
+  public CompletionStage<Cart> addItem(AddItemRequest in) {
+    logger.info("addItem {} to cart {}", in.getItemId(), in.getCartId());
+    EntityRef<ShoppingCart.Command> entityRef =
+        sharding.entityRefFor(ShoppingCart.ENTITY_KEY, in.getCartId());
+    CompletionStage<ShoppingCart.Summary> reply =
+        entityRef.askWithStatus(
+            replyTo -> new ShoppingCart.AddItem(in.getItemId(), in.getQuantity(), replyTo),
+            timeout);
+    CompletionStage<Cart> cart = reply.thenApply(ShoppingCartServiceImpl::toProtoCart);
+    return convertError(cart);
+  }
+
+  @Override
+  public CompletionStage<Cart> updateItem(UpdateItemRequest in) {
+    logger.info("getCart {}", in.getCartId());
+    EntityRef<ShoppingCart.Command> entityRef =
+        sharding.entityRefFor(ShoppingCart.ENTITY_KEY, in.getCartId());
+    final CompletionStage<ShoppingCart.Summary> reply;
+    if (in.getQuantity() == 0) {
+      reply =
+          entityRef.askWithStatus(
+              replyTo -> new ShoppingCart.RemoveItem(in.getItemId(), replyTo), timeout);
+    } else {
+      reply =
+          entityRef.askWithStatus(
+              replyTo ->
+                  new ShoppingCart.AdjustItemQuantity(in.getItemId(), in.getQuantity(), replyTo),
+              timeout);
     }
+    CompletionStage<Cart> cart = reply.thenApply(ShoppingCartServiceImpl::toProtoCart);
+    return convertError(cart);
+  }
 
-    @Override
-    public CompletionStage<Cart> addItem(AddItemRequest in) {
-        logger.info("addItem {} to cart {}", in.getItemId(), in.getCartId());
-        EntityRef<ShoppingCart.Command> entityRef =
-                sharding.entityRefFor(ShoppingCart.ENTITY_KEY, in.getCartId());
-        CompletionStage<ShoppingCart.Summary> reply =
-                entityRef.askWithStatus(
-                        replyTo -> new ShoppingCart.AddItem(in.getItemId(), in.getQuantity(), replyTo),
-                        timeout);
-        CompletionStage<Cart> cart = reply.thenApply(ShoppingCartServiceImpl::toProtoCart);
-        return convertError(cart);
-    }
+  
+  @Override
+  public CompletionStage<Cart> checkout(CheckoutRequest in) {
+    logger.info("checkout {}", in.getCartId());
+    EntityRef<ShoppingCart.Command> entityRef =
+        sharding.entityRefFor(ShoppingCart.ENTITY_KEY, in.getCartId());
+    CompletionStage<ShoppingCart.Summary> reply =
+        entityRef.askWithStatus(replyTo -> new ShoppingCart.Checkout(replyTo), timeout);
+    CompletionStage<Cart> cart = reply.thenApply(ShoppingCartServiceImpl::toProtoCart);
+    return convertError(cart);
+  }
 
-    private static Cart toProtoCart(ShoppingCart.Summary cart) {
-        List<Item> protoItems =
-                cart.items.entrySet().stream()
-                        .map(
-                                entry ->
-                                        Item.newBuilder()
-                                                .setItemId(entry.getKey())
-                                                .setQuantity(entry.getValue())
-                                                .build())
-                        .collect(Collectors.toList());
+  @Override
+  public CompletionStage<Cart> getCart(GetCartRequest in) {
+    logger.info("getCart {}", in.getCartId());
+    EntityRef<ShoppingCart.Command> entityRef =
+        sharding.entityRefFor(ShoppingCart.ENTITY_KEY, in.getCartId());
+    CompletionStage<ShoppingCart.Summary> reply =
+        entityRef.ask(replyTo -> new ShoppingCart.Get(replyTo), timeout);
+    CompletionStage<Cart> protoCart =
+        reply.thenApply(
+            cart -> {
+              if (cart.items.isEmpty())
+                throw new GrpcServiceException(
+                    Status.NOT_FOUND.withDescription("Cart " + in.getCartId() + " not found"));
+              else return toProtoCart(cart);
+            });
+    return convertError(protoCart);
+  }
+  
 
-        return Cart.newBuilder().addAllItems(protoItems).build();
-    }
+  
+  private static Cart toProtoCart(ShoppingCart.Summary cart) {
+    List<Item> protoItems =
+        cart.items.entrySet().stream()
+            .map(
+                entry ->
+                    Item.newBuilder()
+                        .setItemId(entry.getKey())
+                        .setQuantity(entry.getValue())
+                        .build())
+            .collect(Collectors.toList());
 
-    private static <T> CompletionStage<T> convertError(CompletionStage<T> response) {
-        return response.exceptionally(
-                ex -> {
-                    if (ex instanceof TimeoutException) {
-                        throw new GrpcServiceException(
-                                Status.UNAVAILABLE.withDescription("Operation timed out"));
-                    } else {
-                        throw new GrpcServiceException(
-                                Status.INVALID_ARGUMENT.withDescription(ex.getMessage()));
-                    }
-                });
-    }
+    return Cart.newBuilder().setCheckedOut(cart.checkedOut).addAllItems(protoItems).build();
+  }
+  
+
+  private static <T> CompletionStage<T> convertError(CompletionStage<T> response) {
+    return response.exceptionally(
+        ex -> {
+          if (ex instanceof TimeoutException) {
+            throw new GrpcServiceException(
+                Status.UNAVAILABLE.withDescription("Operation timed out"));
+          } else {
+            throw new GrpcServiceException(
+                Status.INVALID_ARGUMENT.withDescription(ex.getMessage()));
+          }
+        });
+  }
 }
